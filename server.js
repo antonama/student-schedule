296684888cddafc5a5d2df2a7b274b07e957bbdc
Anton Abramovich <a-abramovich911@mail.ru>
2015-05-24 15:16:41 +0300

    Change

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
+    console.log("Ok");
 }).listen(port);

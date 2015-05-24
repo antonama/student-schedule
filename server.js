c0bfac6ef3ea1533665eaba4492b430a9da76ec8
Anton Abramovich <a-abramovich911@mail.ru>
2015-05-24 15:27:50 +0300

    Add express

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
  * Created by Anton on 5/24/2015.
  */
 
-var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-    console.log("Ok");
-}).listen(port);
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 1337);
+
+
